ebcc0f047bfdb428200174bfd8bb4ac93dddcaa5
Tomasz Szewczyk <tomasz.szewczyk02@gmail.com>
2020-01-08 22:15:00 +0100

    working code, some unittests

--- src/main/java/pl/edu/agh/distributedsystems/reservationservice/ReservationServiceApp.java
+++ src/main/java/pl/edu/agh/distributedsystems/reservationservice/ReservationServiceApp.java
@@ -1,16 +1,20 @@
-package reservation;
+package pl.edu.agh.distributedsystems.reservationservice;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ReservationServiceApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ReservationServiceApp.class, args);
     }
 
     @Bean
--- src/main/java/pl/edu/agh/distributedsystems/reservationservice/dao/CustomerRepository.java
+++ src/main/java/pl/edu/agh/distributedsystems/reservationservice/dao/CustomerRepository.java
@@ -1,4 +1,4 @@
-package reservation.dao;
+package pl.edu.agh.distributedsystems.reservationservice.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
